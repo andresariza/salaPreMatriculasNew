209edf39527b97b47b767c97a7212f4f2331b758
Andres Ariza <andresgudlu@gmail.com>
2019-03-15 17:00:41 -0500

    primeras interfaces

---
+++ sala/lib/GestorDePrematriculas/interfaces/IPrematricula.php
@@ -0,0 +1,18 @@
+<?php
+namespace Sala\lib\GestorDePrematriculas\interfaces;
+defined('_EXEC') or die;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author arizaandres
+ */
+interface IPrematricula {
+    public function agregrarPrematricula();
+    public function eliminarPrematricula();
+}
